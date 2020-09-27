a7bf7cd76672bd90c0769ed1fbe6f173b53bfb06
Vasanthan <vasanth2754@gmail.com>
2020-09-27 08:54:14 +0530

    Updates

---
+++ precache-manifest.770ef6bdf87a8e0351e266412e11c3a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab6caf964cfe8bf59870f093c0f8584",
+    "url": "/Advice-app/index.html"
+  },
+  {
+    "revision": "401edc5909a4b4694733",
+    "url": "/Advice-app/static/css/main.71aceabd.chunk.css"
+  },
+  {
+    "revision": "59823e49fb7b1630277b",
+    "url": "/Advice-app/static/js/2.c03af8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Advice-app/static/js/2.c03af8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401edc5909a4b4694733",
+    "url": "/Advice-app/static/js/main.24f62a8d.chunk.js"
+  },
+  {
+    "revision": "1ffa4e0e45ac2e80b3eb",
+    "url": "/Advice-app/static/js/runtime-main.6cfa44b5.js"
+  },
+  {
+    "revision": "40c9c91ef4eb9a19ef9a8e17eea02d12",
+    "url": "/Advice-app/static/media/nature.40c9c91e.jpg"
+  }
+]);
